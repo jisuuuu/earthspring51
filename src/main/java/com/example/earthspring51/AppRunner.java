bc1ff2e3cfccb7bced75ab78194526b8cd35705d
jisuuuu <gjkl55@naver.com>
2022-11-29 20:12:20 +0900

    Environment - 바로 Profile 등록하는 방법

--- src/main/java/com/example/earthspring51/AppRunner.java
+++ src/main/java/com/example/earthspring51/AppRunner.java
@@ -4,23 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
-<<<<<<< HEAD
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
-=======
-import org.springframework.stereotype.Component;
-
->>>>>>> origin/master
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
     ApplicationContext ctx;
 
-<<<<<<< HEAD
     @Autowired
     BookRepository bookRepository;
 
@@ -30,26 +24,5 @@ public class AppRunner implements ApplicationRunner {
         Environment environment = ctx.getEnvironment();
         System.out.println(Arrays.toString(environment.getActiveProfiles()));
         System.out.println(Arrays.toString(environment.getDefaultProfiles()));
-=======
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        System.out.println("proto");
-
-        System.out.println(ctx.getBean(Proto.class));
-        System.out.println(ctx.getBean(Proto.class));
-        System.out.println(ctx.getBean(Proto.class));
-
-        System.out.println("single");
-
-        System.out.println(ctx.getBean(Single.class));
-        System.out.println(ctx.getBean(Single.class));
-        System.out.println(ctx.getBean(Single.class));
-
-        System.out.println("proto by single");// proto를 proxy로 감싸서 바로 사용하지 못하도록, class 기반 proxy로 만들어서 사용
-
-        System.out.println(ctx.getBean(Single.class).getProto());
-        System.out.println(ctx.getBean(Single.class).getProto());
-        System.out.println(ctx.getBean(Single.class).getProto());
->>>>>>> origin/master
     }
 }
---
+++ src/main/java/com/example/earthspring51/BookRepository.java
@@ -0,0 +1,4 @@
+package com.example.earthspring51;
+
+public interface BookRepository {
+}
