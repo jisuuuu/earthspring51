2a088ce1d2bcdd2bab98cf86ad6084f6dde32888
jisuuuu <gjkl55@naver.com>
2022-11-29 20:07:14 +0900

    Environment - 바로 Profile 등록하는 방법

--- src/main/java/com/example/earthspring51/TestBookRepository.java
+++ src/main/java/com/example/earthspring51/TestBookRepository.java
@@ -1,4 +1,9 @@
 package com.example.earthspring51;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Profile("!prod")
 public class TestBookRepository implements BookRepository{
 }
